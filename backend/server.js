38303cde42540762f3aafedb64af84dedb45ee9c
gcarloin2002 <gcarloin2002@gmail.com>
2023-07-08 07:14:11 -0500

    added basic backend files

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express()
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
