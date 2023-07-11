5b20b2f5146a3246f16772760a66860f0d806661
gcarloin2002 <gcarloin2002@gmail.com>
2023-07-11 07:07:53 -0500

    adjust image files

--- frontend/src/components/ButtonCollection.js
+++ frontend/src/components/ButtonCollection.js
@@ -2,7 +2,10 @@ import DexButton from "./DexButton"
 import "./ButtonCollection.css"
 
 const ButtonCollection = (props) => {
-    const pkmn = props.pkmn
+    const pkmn = props.pkmn // list of every name and url of pokemon
+    const imageList = require.context('../images/standard', false, /\.(png)$/).keys(); // List of standard image urls
+    console.log(imageList)
+
 
     return (
         <div className="DexButtons">
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import TestComponent from "../components/TestComponent"
 import ButtonCollection from "../components/ButtonCollection"
 import { useEffect, useState } from "react"
-import image from "../images/home/1.png"
+
 
 const Home = () => {
     // Array of every pokemon's data
@@ -26,7 +26,6 @@ const Home = () => {
     return (
         <div>
             <h2>Home</h2>
-            <img src={image}></img>
             <ButtonCollection pkmn={pokemon}/>
         </div>
     )
