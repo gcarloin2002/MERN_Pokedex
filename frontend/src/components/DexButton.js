af176cde3cbd1b24f6b7c7fb305672b1df0622d6
gcarloin2002 <gcarloin2002@gmail.com>
2023-07-11 16:13:18 -0500

    adjust display names

--- frontend/src/components/DexButton.js
+++ frontend/src/components/DexButton.js
@@ -3,12 +3,53 @@ import "./DexButton.css"
 
 const DexButton = (props) => {
     const data = props.data
-    const dexNumber = "#" + props.dexNumber
-    const origName = data.name
-    const displayName = origName.charAt(0).toUpperCase() + origName.slice(1)
     const url = data.url 
     const imageFilepath = props.imgSrc
-    
+    const dexNumber = "#" + props.dexNumber
+    const origName = data.name
+
+    const protoDisplayName = origName.charAt(0).toUpperCase() + origName.slice(1)
+    const determineDisplayName = (name) => {
+        switch (name) {
+            case "Nidoran-f":
+                return "Nidoran♀"
+            case "Nidoran-m":
+                return "Nidoran♂"
+            case "Mr-mime":
+                return "Mr. Mime"
+            case "Ho-oh":
+                return "Ho-Oh"
+            case "Deoxys-normal":
+                return "Deoxys"
+            case "Wormadam-plant":
+                return "Wormadam"
+            case "Mime-jr":
+                return "Mime Jr."
+            case "Porygon-z":
+                return "Porygon-Z"
+            case "Giratina-altered":
+                return "Giratina"
+            case "Shaymin-land":
+                return "Shaymin"
+            case "Basculin-red-striped":
+                return "Basculin"
+            case "Darmanitan-standard":
+                return "Darmanitan"
+            case "Tornadus-incarnate": 
+                return "Tornadus"
+            case "Thundurus-incarnate":
+                return "Thundurus"
+            case "Landorus-incarnate":
+                return "Landorus"
+
+
+
+            default: 
+                return name
+        }
+    }
+
+    const displayName = determineDisplayName(protoDisplayName)
     
     return (
     <div className="DexButton" key={props.key}>
