285283be55fb24be356926f517649c4a01371020
gcarloin2002 <gcarloin2002@gmail.com>
2023-07-12 04:47:02 -0500

    added new image and adjust import statements

--- frontend/src/components/DexButton.js
+++ frontend/src/components/DexButton.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react"
 import "./styles/DexButton.css"
 
 const determineDisplayName = (name, dexNumber) => {
--- frontend/src/components/TestComponent.js
+++ frontend/src/components/TestComponent.js
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from "react"
 
 const TestComponent = () => {
-    const [names, setNames] = useState([]);
-    
-    const response = fetch("https://pokeapi.co/api/v2/pokemon?limit=1010")
-    .then((response) => response.json())
-    .then((json) => json.results)
-    .then((result) => {
-        const n = []
-        for (let i = 0; i < result.length; i++){
-            n.push(result[i].name.charAt(0).toUpperCase() + result[i].name.slice(1))
-        }
-        setNames(n)
-    })
-
-    const listItems = names.map((n) => 
-        <li key={n.toString()}>{n}</li>
-    )
-
-
     return (
         <div>
-            {listItems}
+            TopBar
         </div>
     )
 }
