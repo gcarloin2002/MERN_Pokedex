ec5b13071fd511f44aeda0614cf01fc5b257c579
gcarloin2002 <gcarloin2002@gmail.com>
2023-07-14 10:19:52 -0500

    added way to return to homepage

--- frontend/src/components/DirectoryBar.js
+++ frontend/src/components/DirectoryBar.js
@@ -1,18 +1,16 @@
 import "./styles/DirectoryBar.css"
 import leftArrow from "../assets/gui/leftArrow.png"
 import rightArrow from "../assets/gui/rightArrow.png"
+import { useNavigate } from "react-router-dom"
 
 const DirectoryBar = (props) => {
     const dexNum = props.dexNum
+    const navigate = useNavigate()
     const leftPic = (dexNum) => {
         if (dexNum !== 1){
             return (
             <div className="leftContainer">
-                <img 
-                    className="leftArrowPic"
-                    src={leftArrow}
-                    alt={""}
-                />
+                <img className="leftArrowPic" src={leftArrow} alt={""}/>
                 <img 
                     className="leftPokemonPic" 
                     src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + (dexNum - 1) + ".png"} 
@@ -28,11 +26,7 @@ const DirectoryBar = (props) => {
             return (
             <div className="rightContainer">
                 <img 
-                    className="rightArrowPic"
-                    src={rightArrow}
-                    alt={""}
-                />
-                
+                    className="rightArrowPic" src={rightArrow} alt={""}/>
                 <img 
                     className="rightPokemonPic" 
                     src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + (dexNum + 1) +".png"} 
@@ -43,10 +37,17 @@ const DirectoryBar = (props) => {
         }
     }
 
+    const handleCentralContainerClick = () => {
+        navigate("/")
+    }
+
+
     return (
         <div className="DirectoryBar">
             {leftPic(dexNum)}
-            <div className="centralContainer"></div>
+            <div className="centralContainer" onClick={handleCentralContainerClick}>
+                <p className="centralContainerText">return to Homepage</p>
+            </div>
             {rightPic(dexNum)}
         </div>
     )
