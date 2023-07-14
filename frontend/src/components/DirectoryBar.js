9792e0a93cd26d7d7a69623903b3b95c794a01ed
gcarloin2002 <gcarloin2002@gmail.com>
2023-07-14 11:54:00 -0500

    added navigations to directory bar

--- frontend/src/components/DirectoryBar.js
+++ frontend/src/components/DirectoryBar.js
@@ -3,52 +3,62 @@ import leftArrow from "../assets/gui/leftArrow.png"
 import rightArrow from "../assets/gui/rightArrow.png"
 import { useNavigate } from "react-router-dom"
 
+const leftPic = (dexNum, handleLeftContainerClick) => {
+    if (dexNum !== 1){
+        return (
+        <div className="leftContainer" onClick={handleLeftContainerClick}>
+            <img className="leftArrowPic" src={leftArrow} alt={""}/>
+            <img 
+                className="leftPokemonPic" 
+                src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + (dexNum - 1) + ".png"} 
+                alt={""}
+            />
+        </div>
+        )
+    }
+}
+
+const rightPic = (dexNum, handleleRightContainerClick) => {
+    if (dexNum !== 1010){
+        return (
+        <div className="rightContainer" onClick={handleleRightContainerClick}>
+            <img 
+                className="rightArrowPic" src={rightArrow} alt={""}/>
+            <img 
+                className="rightPokemonPic" 
+                src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + (dexNum + 1) +".png"} 
+                alt={""}
+            />
+        </div>
+        )
+    }
+}
+
+// Component
 const DirectoryBar = (props) => {
     const dexNum = props.dexNum
     const neighbors = props.neighbors
     const navigate = useNavigate()
-    const leftPic = (dexNum) => {
-        if (dexNum !== 1){
-            return (
-            <div className="leftContainer">
-                <img className="leftArrowPic" src={leftArrow} alt={""}/>
-                <img 
-                    className="leftPokemonPic" 
-                    src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + (dexNum - 1) + ".png"} 
-                    alt={""}
-                />
-            </div>
-            )
-        }
-    }
-    
-    const rightPic = (dexNum) => {
-        if (dexNum !== 1010){
-            return (
-            <div className="rightContainer">
-                <img 
-                    className="rightArrowPic" src={rightArrow} alt={""}/>
-                <img 
-                    className="rightPokemonPic" 
-                    src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + (dexNum + 1) +".png"} 
-                    alt={""}
-                />
-            </div>
-            )
-        }
-    }
 
     const handleCentralContainerClick = () => {
         navigate("/")
     }
 
+    const handleLeftContainerClick = () => {
+        navigate("/" + neighbors[0])
+    }
+
+    const handleleRightContainerClick = () => {
+        navigate("/" + neighbors[1])
+    }
+
     return (
         <div className="DirectoryBar">
-            {leftPic(dexNum)}
+            {leftPic(dexNum, handleLeftContainerClick)}
             <div className="centralContainer" onClick={handleCentralContainerClick}>
                 <p className="centralContainerText">return to Homepage</p>
             </div>
-            {rightPic(dexNum)}
+            {rightPic(dexNum, handleleRightContainerClick)}
         </div>
     )
 }
