d07ac0f139826d2b2444adab62897cbac2e25ddd
gcarloin2002 <gcarloin2002@gmail.com>
2023-07-20 18:54:40 -0500

    finished gender ratio bar

--- frontend/src/components/PokedexEntryComponents/DemographicDisplay.js
+++ frontend/src/components/PokedexEntryComponents/DemographicDisplay.js
@@ -1,4 +1,5 @@
 
+import "./styles/DemographicDisplay.css"
 
 const generateGenderRates = (speciesData) => {
     const gender_rate = speciesData.gender_rate
@@ -16,11 +17,75 @@ const generateGenderRates = (speciesData) => {
     }
 }
 
+const generateGenderBarDiv = (genderRates) => {
+    if (genderRates.male === 0 && genderRates.female === 0) {
+        return (
+            <div className="genderContainer">
+                <div className="genderBar" style={{"justifyContent": "center", "backgroundColor": "black"}}>
+                    <div className="certainNum">Gender Unknown</div>
+                </div>
+            </div>
+        )
+    }
+
+    else if (genderRates.male === 100 && genderRates.female === 0) {
+        return (
+            <div className="genderContainer">
+                <div className="genderBar" style={{"justifyContent": "center", "backgroundColor": "#3355FF"}}>
+                    <div className="certainNum">{genderRates.male + "% ♂"}</div>
+                </div>
+            </div>
+            
+        )
+    }
+
+    else if (genderRates.male === 0 && genderRates.female === 100) {
+        return (
+            <div className="genderContainer">
+                <div className="genderBar" style={{"justifyContent": "center", "backgroundColor": "#FF77DD"}}>
+                    <div className="certainNum">{genderRates.female + "% ♂"}</div>
+                </div>
+            </div>
+        )
+    }
+
+    else {
+        // ["#3355FF","#FF77DD"]
+        const mCol = "#3355FF"
+        const fCol = "#FF77DD"
+        const mRate = genderRates.male
+        const fRate = genderRates.female
+        const contentColors = ["".concat(mCol, " 0%"), "".concat(mCol, " ", mRate, "%"), "".concat(fCol, " ", mRate, "%"), "".concat(fCol, " 100%")]
+        
+
+        const contentGradient = (contentColors).join(", ")
+        console.log(contentGradient)
+        const style = {background:"linear-gradient(to right," + contentGradient + ") padding-box"}
+
+
+        return (
+            <div className="genderContainer">
+                <div className="maleNum">{mRate + "% ♂"}</div>
+                <div className="genderBar" style={style}></div>
+                <div className="femaleNum">{fRate + "% ♀"}</div>
+            </div>
+        )
+    }
+
+    
+}
+
 const DemographicDisplay = (props) => {
+    const style = props.style
+    const speciesData = props.speciesData
+    const genderRates = generateGenderRates(speciesData)
     
     return (
-        <div>
-            DemographicDisplay
+        <div className="DemographicDisplay">
+            {generateGenderBarDiv(genderRates)}
+            <div className="heightWeightContainer">
+
+            </div>
         </div>
     )
 }
